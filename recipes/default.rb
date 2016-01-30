a9dfe7b56b4e76f49170d4bb8343d56725d5a739
Katsuya <katsuya.f0515@gmail.com>
2016-01-30 15:49:30 +0900

    my fisrt cookbook

---
+++ recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+template "nginx.conf" do
+        path "/etc/nginx/nginx.conf"
+        source "nginx.conf.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        notifies :reload,'service[nginx]'
+end
